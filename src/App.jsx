f589a23916996f3232fbf276a9854a5d7f5a8d3b
Daniel Neto <me@danielneto.com>
2025-03-28 19:23:53 -0300

    Add favico

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import VideoGallery from "./components/Video/VideoGallery";
 import TopBar from "./components/UI/TopBar";
 import FullPageLoader from "./components/UI/FullPageLoader"; 
@@ -18,6 +18,13 @@ function App() {
         setIsLoggedIn(false);
         localStorage.removeItem("isLoggedIn");
     };
+
+    useEffect(() => {
+        const favicon = document.querySelector("link[rel~='icon']");
+        if (favicon) {
+          favicon.href = import.meta.env.VITE_BASE_URL + "videos/favicon.ico";
+        }
+      }, []);
 
     return (
         <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300">
